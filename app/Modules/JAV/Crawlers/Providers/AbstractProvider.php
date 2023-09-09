f5cdf49b67ca1c1eec39d95c6d0006dcf2e561d9
Viet Vu <jooservices@gmail.com>
2023-09-09 09:16:08 +0700

    tmp

---
+++ app/Modules/JAV/Crawlers/Providers/AbstractProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\JAV\Crawlers\Providers;
+
+abstract class AbstractProvider implements CrawlerProviderInterface
+{
+    public function __construct() {
+
+    }
+}
---
+++ app/Modules/JAV/Crawlers/Providers/Onejav/OnejavItems.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Modules\JAV\Crawlers\Providers\Onejav;
+
+class OnejavItems implements \App\Modules\JAV\Crawlers\Providers\CrawlerProviderInterface
+{
+
+    public function crawl(string $url, array $data = [])
+    {
+        // TODO: Implement crawl() method.
+    }
+}
