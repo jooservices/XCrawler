31ca38826b6610305df320f5a2efe3d4e3000778
Viet Vu <jooservices@gmail.com>
2023-09-20 14:30:44 +0700

    Update

---
+++ app/Modules/Core/Jobs/BaseJob.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Modules\Core\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+
+class BaseJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+
+    public $tries = 10;
+
+    public $timeout = 60;
+
+    public $retryAfter = 120;
+}
