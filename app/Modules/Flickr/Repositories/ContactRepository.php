b1e74b240a99ae6089946109fc24041182f70d48
Viet Vu <jooservices@gmail.com>
2023-09-21 18:15:00 +0700

    Hotfix orderBy for Flickr

--- app/Modules/Flickr/Repositories/ContactRepository.php
+++ app/Modules/Flickr/Repositories/ContactRepository.php
@@ -32,6 +32,7 @@ class ContactRepository extends CrudRepository
                 return $query->where('state_code', '!=', States::STATE_IN_PROGRESS)
                     ->where('state_code', '!=', States::STATE_COMPLETED);
             })
+            ->orderBy('created_at')
             ->limit($limit)
             ->get();
 
@@ -54,6 +55,7 @@ class ContactRepository extends CrudRepository
                 return $query->where('favorites_state_code', '!=', States::STATE_IN_PROGRESS)
                     ->where('favorites_state_code', '!=', States::STATE_COMPLETED);
             })
+            ->orderBy('created_at')
             ->limit($limit)
             ->get();
 
