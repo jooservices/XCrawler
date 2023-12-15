6f00f9bf0e7e9f528489bdd859a1d4df1074c03b
Viet Vu <jooservices@gmail.com>
2023-12-15 12:38:06 +0700

    LINT

--- app/Modules/Flickr/Console/MigrateContacts.php
+++ app/Modules/Flickr/Console/MigrateContacts.php
@@ -2,7 +2,6 @@
 
 namespace App\Modules\Flickr\Console;
 
-use App\Modules\Client\Repositories\IntegrationRepository;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -31,30 +30,36 @@ class MigrateContacts extends Command
      */
     public function handle(): void
     {
-        $now = Carbon::now();
+        $mysqlConnection = DB::connection('mysql');
         foreach (DB::connection('mongodb')->table('flickr_contacts')->cursor() as $contact) {
             $this->output->writeln('Processing contact ' . $contact['nsid']);
-            DB::connection('mysql')->table('flickr_contacts')->insert([
-                'uuid' => Str::orderedUuid(),
-                'nsid' => $contact['nsid'],
-                'username' => $contact['username'] ?? null,
-                'realname' => $contact['realname'] ?? null,
-                'friend' => $contact['friend'] ?? null,
-                'family' => $contact['family'] ?? null,
-                'ignored' => $contact['ignored'] ?? null,
-                'rev_ignored' => $contact['rev_ignored'] ?? null,
-                'iconserver' => $contact['iconserver'] ?? null,
-                'iconfarm' => $contact['iconfarm'] ?? null,
-                'path_alias' => $contact['path_alias'] ?? null,
-                'location' => $contact['location'] ?? null,
-                'description' => $contact['description'] ?? null,
-                'photosurl' => $contact['photosurl'] ?? null,
-                'profileurl' => $contact['profileurl'] ?? null,
-                'mobileurl' => $contact['mobileurl'] ?? null,
-
-                'created_at' => $contact['created_at'] ? $now : null,
-                'updated_at' => $contact['updated_at'] ? $now : null,
-            ]);
+            $mysqlConnection->table('flickr_contacts')->insert($this->mapping($contact));
         }
+    }
+
+    private function mapping(array $contact): array
+    {
+        $now = Carbon::now();
+        return [
+            'uuid' => Str::orderedUuid(),
+            'nsid' => $contact['nsid'],
+            'username' => $contact['username'] ?? null,
+            'realname' => $contact['realname'] ?? null,
+            'friend' => $contact['friend'] ?? null,
+            'family' => $contact['family'] ?? null,
+            'ignored' => $contact['ignored'] ?? null,
+            'rev_ignored' => $contact['rev_ignored'] ?? null,
+            'iconserver' => $contact['iconserver'] ?? null,
+            'iconfarm' => $contact['iconfarm'] ?? null,
+            'path_alias' => $contact['path_alias'] ?? null,
+            'location' => $contact['location'] ?? null,
+            'description' => $contact['description'] ?? null,
+            'photosurl' => $contact['photosurl'] ?? null,
+            'profileurl' => $contact['profileurl'] ?? null,
+            'mobileurl' => $contact['mobileurl'] ?? null,
+
+            'created_at' => $contact['created_at'] ? $now : null,
+            'updated_at' => $contact['updated_at'] ? $now : null,
+        ];
     }
 }
