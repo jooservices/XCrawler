91397951896c0746c19fb3db8cd1b0100ae8cf46
Viet Vu <jooservices@gmail.com>
2023-12-17 08:46:54 +0700

    Lint

--- app/Modules/Flickr/Console/MigrateContacts.php
+++ app/Modules/Flickr/Console/MigrateContacts.php
@@ -30,33 +30,39 @@ class MigrateContacts extends Command
      */
     public function handle(): void
     {
-        $now = Carbon::now();
-        $items = DB::connection('mongodb')->table('flickr_contacts')->limit(1000)->get();
-        $items = $items->map(function ($item) use ($now) {
-            return [
-                'uuid' => Str::orderedUuid(),
-                'nsid' => $item['nsid'],
-                'username' => $item['username'] ?? null,
-                'realname' => $item['realname'] ?? null,
-                'friend' => $item['friend'] ?? null,
-                'family' => $item['family'] ?? null,
-                'ignored' => $item['ignored'] ?? null,
-                'rev_ignored' => $item['rev_ignored'] ?? null,
-                'iconserver' => $item['iconserver'] ?? null,
-                'iconfarm' => $item['iconfarm'] ?? null,
-                'path_alias' => $item['path_alias'] ?? null,
-                'location' => $item['location'] ?? null,
-                'description' => $item['description'] ?? null,
-                'photosurl' => $item['photosurl'] ?? null,
-                'profileurl' => $item['profileurl'] ?? null,
-                'mobileurl' => $item['mobileurl'] ?? null,
 
-                'created_at' => $item['created_at'] ? $now : null,
-                'updated_at' => $item['updated_at'] ? $now : null,
-            ];
+        $items = DB::connection('mongodb')->table('flickr_contacts')->limit(1000)->get();
+        $items = $items->map(function ($item) {
+            return $this->mapping($item);
         });
 
         $items = $items->unique('nsid');
         DB::connection('mysql')->table('flickr_contacts')->insert($items->toArray());
+    }
+
+    private function mapping(array $item): array
+    {
+        $now = Carbon::now();
+        return [
+            'uuid' => Str::orderedUuid(),
+            'nsid' => $item['nsid'],
+            'username' => $item['username'] ?? null,
+            'realname' => $item['realname'] ?? null,
+            'friend' => $item['friend'] ?? null,
+            'family' => $item['family'] ?? null,
+            'ignored' => $item['ignored'] ?? null,
+            'rev_ignored' => $item['rev_ignored'] ?? null,
+            'iconserver' => $item['iconserver'] ?? null,
+            'iconfarm' => $item['iconfarm'] ?? null,
+            'path_alias' => $item['path_alias'] ?? null,
+            'location' => $item['location'] ?? null,
+            'description' => $item['description'] ?? null,
+            'photosurl' => $item['photosurl'] ?? null,
+            'profileurl' => $item['profileurl'] ?? null,
+            'mobileurl' => $item['mobileurl'] ?? null,
+
+            'created_at' => $item['created_at'] ? $now : null,
+            'updated_at' => $item['updated_at'] ? $now : null,
+        ];
     }
 }
