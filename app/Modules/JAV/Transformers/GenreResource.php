9703b432a6ef63d62d4078d20944d2fab4612ecb
Viet Vu <jooservices@gmail.com>
2023-12-17 15:42:58 +0700

    SonarCloud

--- app/Modules/JAV/Transformers/GenreResource.php
+++ app/Modules/JAV/Transformers/GenreResource.php
@@ -6,14 +6,4 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class GenreResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
 }
