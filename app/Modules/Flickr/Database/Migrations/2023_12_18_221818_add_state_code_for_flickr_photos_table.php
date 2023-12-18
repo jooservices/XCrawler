4fd621898e6b4341a728349e0d49578087d80c65
Viet Vu <jooservices@gmail.com>
2023-12-19 05:33:18 +0700

    Update PhotoSizes job

---
+++ app/Modules/Flickr/Database/Migrations/2023_12_18_221818_add_state_code_for_flickr_photos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('flickr_photos', function (Blueprint $table) {
+            $table->string('state_code')->nullable()->after('media')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+            $table->dropColumn('state_code');
+        });
+    }
+};
