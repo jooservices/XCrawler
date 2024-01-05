7919cdc81bed77afa734797636a75fa9f8b01427
Viet Vu <jooservices@gmail.com>
2024-01-05 14:00:48 +0700

    Update download file

--- app/Modules/Flickr/Tests/Feature/Jobs/DownloadPhotoJobTest.php
+++ app/Modules/Flickr/Tests/Feature/Jobs/DownloadPhotoJobTest.php
@@ -2,10 +2,7 @@
 
 namespace App\Modules\Flickr\Tests\Feature\Jobs;
 
-use App\Modules\Flickr\Events\ContactCreatedEvent;
-use App\Modules\Flickr\Events\FetchContactsCompletedEvent;
-use App\Modules\Flickr\Events\FetchContactsRecursiveEvent;
-use App\Modules\Flickr\Jobs\ContactsJob;
+use App\Modules\Flickr\Events\PhotosetPhotoDownloadCompletedEvent;
 use App\Modules\Flickr\Jobs\DownloadPhotoJob;
 use App\Modules\Flickr\Models\FlickrPhoto;
 use App\Modules\Flickr\Services\FlickrService;
@@ -16,6 +13,7 @@ class DownloadPhotoJobTest extends TestCase
 {
     public function testDownload()
     {
+        Event::fake(PhotosetPhotoDownloadCompletedEvent::class);
         $photo = FlickrPhoto::factory()->create([
             'sizes' => [
                 [
@@ -25,9 +23,14 @@ class DownloadPhotoJobTest extends TestCase
         ]);
 
         $task = $photo->tasks()->create([
-            'task' => FlickrService::TASK_DOWNLOAD_PHOTOSET_PHOTO
+            'task' => FlickrService::TASK_DOWNLOAD_PHOTOSET_PHOTO,
+            'payload' => [
+                'photos' => 1
+            ]
         ]);
 
         DownloadPhotoJob::dispatch($task);
+
+        Event::assertDispatched(PhotosetPhotoDownloadCompletedEvent::class);
     }
 }
