a3e38888d570743b4756263ee2c51debd2cf4e51
Viet Vu <jooservices@gmail.com>
2024-01-05 11:00:53 +0700

    Update Tests

--- app/Modules/Client/Repositories/IntegrationRepository.php
+++ app/Modules/Client/Repositories/IntegrationRepository.php
@@ -12,53 +12,6 @@ use NunoMaduro\Collision\Adapters\Phpunit\State;
 
 class IntegrationRepository
 {
-    /**
-     * @throws NoIntegrateException
-     */
-    public function getItem(string $service, ?string $name = null, ?string $stateCode = null): Integration
-    {
-        $integration =  Integration::where('service', $service)
-            ->when($name, function ($query, $name) {
-                $query->where('name', $name);
-            }, function ($query) {
-                $query->where('is_primary', true);
-            })
-            ->when($stateCode, function ($query, $stateCode) {
-                $query->where('state_code', $stateCode);
-            })->first();
-
-        if (!$integration) {
-            throw new NoIntegrateException('There is no integration');
-        }
-
-        return $integration;
-    }
-    public function getIntegration(string $service, string $name = null): Integration
-    {
-        $integration = Integration::where('service', $service)
-            ->where('name', $name)
-            ->first();
-
-        if (!$integration) {
-            throw new NoIntegrateException('There is no integration');
-        }
-
-        return $integration;
-    }
-
-    public function getItems(string $service, ?string $name = null, ?string $stateCode = null): Collection
-    {
-        return Integration::where('service', $service)
-            ->when($stateCode !== null, function ($query, $stateCode) {
-                $query->where('state_code', $stateCode);
-            })
-            ->when($name, function ($query, $name) {
-                $query->where('name', $name);
-            }, function ($query) {
-                $query->where('is_primary', true);
-            })->get();
-    }
-
     public function getCompleted(string $service): Collection
     {
         return Integration::where('service', $service)
