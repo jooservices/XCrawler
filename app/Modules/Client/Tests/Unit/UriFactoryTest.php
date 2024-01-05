72f8f9ee77c5b1ed37d1d7eb1085076be4a984d5
Viet Vu <jooservices@gmail.com>
2024-01-05 15:46:13 +0700

    Adding Tests

---
+++ app/Modules/Client/Tests/Unit/UriFactoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Modules\Client\Tests\Unit;
+
+use App\Modules\Client\Tests\TestCase;
+use App\Modules\Client\Uri\UriFactory;
+
+class UriFactoryTest extends TestCase
+{
+    public function testCreateUrl()
+    {
+        $factory = app(UriFactory::class);
+        $uri = $factory->createFromAbsolute('https://example.com');
+
+        $this->assertEquals('https', $uri->getScheme());
+        $this->assertEquals('example.com', $uri->getHost());
+        $this->assertEquals('/', $uri->getPath());
+        $this->assertEquals('', $uri->getQuery());
+        $this->assertEquals('', $uri->getFragment());
+        $this->assertEquals('', $uri->getUserInfo());
+        $this->assertEquals('example.com', $uri->getAuthority());
+        $this->assertEquals(443, $uri->getPort());
+        $this->assertEquals('https://example.com', $uri->getAbsoluteUri());
+    }
+
+    public function testCreateFromParts()
+    {
+        $factory = app(UriFactory::class);
+        $uri = $factory->createFromParts('https', '', 'example.com', 443, '/', '');
+
+        $this->assertEquals('https', $uri->getScheme());
+        $this->assertEquals('example.com', $uri->getHost());
+        $this->assertEquals('/', $uri->getPath());
+        $this->assertEquals('', $uri->getQuery());
+        $this->assertEquals('', $uri->getFragment());
+        $this->assertEquals('', $uri->getUserInfo());
+        $this->assertEquals('example.com', $uri->getAuthority());
+        $this->assertEquals(443, $uri->getPort());
+        $this->assertEquals('https://example.com/', $uri->getAbsoluteUri());
+    }
+}
