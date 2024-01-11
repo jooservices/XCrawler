f21bf4159e1da577a9b51dd6f6baa162e2cc8e82
Viet Vu <jooservices@gmail.com>
2024-01-11 20:53:19 +0700

    Update

---
+++ app/Modules/Core/Database/Migrations/2024_01_11_135250_relationship_task_id_table_tasks.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreign('task_id')
+                ->references('id')
+                ->on('tasks')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+};
