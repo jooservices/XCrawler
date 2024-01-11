640fd6f98214807f62529ec76ab85745b2a792fc
Viet Vu <jooservices@gmail.com>
2024-01-11 20:26:00 +0700

    Update

--- app/Modules/Core/Database/Migrations/2024_01_03_020033_add_task_id_for_tasks_table.php
+++ app/Modules/Core/Database/Migrations/2024_01_03_020033_add_task_id_for_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->unsignedBigInteger('task_id')->default(null)->nullable()->after('id');
+            $table->unsignedBigInteger('task_id')->nullable()->after('id');
         });
     }
 };
