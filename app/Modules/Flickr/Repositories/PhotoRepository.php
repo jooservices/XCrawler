25613dadd1a0ab5f743d5505dbf0d158367e7486
Viet Vu <jooservices@gmail.com>
2024-01-14 12:04:44 +0700

    Fix photo state

--- app/Modules/Flickr/Repositories/PhotoRepository.php
+++ app/Modules/Flickr/Repositories/PhotoRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Modules\Flickr\Repositories;
 
-use App\Modules\Core\Services\States;
 use App\Modules\Core\StateMachine\Task\InProgressState;
 use App\Modules\Flickr\Models\FlickrPhoto;
 use Illuminate\Support\Collection;
@@ -12,7 +11,6 @@ class PhotoRepository
     public function getNoSizesPhotos(int $limit = 10): Collection
     {
         $photos = FlickrPhoto::whereNull('sizes')
-            ->whereNull('state_code')
             ->limit($limit)
             ->get();
 
