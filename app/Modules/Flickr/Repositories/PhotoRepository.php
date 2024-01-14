7613ae2d19858aad9bf0d67f02289f331874d94a
Viet Vu <jooservices@gmail.com>
2024-01-14 14:35:32 +0700

    Update

--- app/Modules/Flickr/Repositories/PhotoRepository.php
+++ app/Modules/Flickr/Repositories/PhotoRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Modules\Flickr\Repositories;
 
-use App\Modules\Core\StateMachine\Task\InProgressState;
 use App\Modules\Flickr\Models\FlickrPhoto;
 use Illuminate\Support\Collection;
 
@@ -10,15 +9,8 @@ class PhotoRepository
 {
     public function getNoSizesPhotos(int $limit = 10): Collection
     {
-        $photos = FlickrPhoto::whereNull('sizes')
+        return FlickrPhoto::whereNull('sizes')
             ->limit($limit)
             ->get();
-
-        FlickrPhoto::whereIn('id', $photos->pluck('id'))
-            ->update([
-                'state_code' => InProgressState::class
-            ]);
-
-        return $photos;
     }
 }
